fc79deea4380eca3ccf1e984a00761b52be9b03c
S4UL0 <90044415+saulooficinas@users.noreply.github.com>
2021-10-16 17:38:59 -0300

    Add files via upload

---
+++ nodemcu/conexao.php
@@ -0,0 +1,15 @@
+<?php
+	try{
+		$HOST ="bsxhgkaksohfxjkryiij-mysql.services.clever-cloud.com";
+		$BANCO="bsxhgkaksohfxjkryiij";
+		$USUARIO="uhsel6hckcsczsdo";
+		$SENHA="rFYEjyUKSlOuLxpr20wk";
+
+		$PDO = new PDO("mysql:host=".$HOST.";dbname=".$BANCO.";charset=utf8",$USUARIO,$SENHA);
+
+	}catch(PDOException $erro){
+
+		//echo "Erro de conexao, detalhes: " .$erro->getMessage();
+		echo "conexao_erro";
+	}
+?>
